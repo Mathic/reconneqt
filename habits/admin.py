6add61d6fe31a6fd9d115d99824f6cd434f09571
Mathic <rizbalt@gmail.com>
2018-09-09 18:42:31 -0400

    register new models

--- habits/admin.py
+++ habits/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Habit, Action, Motive
+from .models import *
 
 # Register your models here.
+admin.site.register(JournalEntry)
+
+admin.site.register(Mood)
+admin.site.register(MoodTracking)
+
 admin.site.register(Habit)
 admin.site.register(Action)
 admin.site.register(Motive)
